ba304d8d8eb89a91bde3ca0244e34db898469954
cambrian <969553988@qq.com>
2019-05-04 23:10:14 +0800

    异常丢失

--- src/net/htwater/think/D12_03/LostMessage.java
+++ src/net/htwater/think/D12_03/LostMessage.java
@@ -4,6 +4,26 @@ package net.htwater.think.D12_03;
  * Created by cambrian on 2019/5/4.
  */
 public class LostMessage {
+    private static void f() throws VeryImportantException{
+        throw new VeryImportantException();
+    }
+
+    private static void dispose() throws NormalException{
+        throw new NormalException();
+    }
+
+    public static void main(String[] args){
+        try {
+            try {
+                f();
+            }finally {
+                dispose();
+            }
+        }catch (Exception e){
+            System.out.println(e);
+        }
+    }
+
 }
 
 class VeryImportantException extends Exception{
