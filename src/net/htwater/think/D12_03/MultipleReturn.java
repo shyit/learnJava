8db21a9cd8019d0f0671cb43b0db3fc792529452
96955 <qwe123>
2019-05-06 15:51:01 +0800

    通过异常处理错误 使用finallly进行清理

---
+++ src/net/htwater/think/D12_03/MultipleReturn.java
@@ -0,0 +1,34 @@
+package net.htwater.think.D12_03;
+
+/**
+ * Created by 96955 on 2019/4/30.
+ */
+public class MultipleReturn {
+
+    public static void f(int i){
+        System.out.println("Initialzation the requires cleanup");
+        try {
+            System.out.println("Point 1");
+            if (i==1){
+                return;
+            }
+            System.out.println("Point 2");
+            if (i==2){
+                return;
+            }
+            System.out.println("Point 3");
+            if (i==3){
+                return;
+            }
+            System.out.println("Point End");
+        }finally {
+            System.out.println("finally");
+        }
+    }
+
+    public static void main(String[] args) {
+        for (int i=1;i<=4;i++){
+            f(i);
+        }
+    }
+}
---
+++ src/net/htwater/think/D12_03/WithFinally.java
@@ -0,0 +1,38 @@
+package net.htwater.think.D12_03;
+
+/**
+ * Created by 96955 on 2019/4/30.
+ */
+public class WithFinally {
+    private static Switch sw = new Switch();
+
+    public static void f() throws OnOffException1, OnOffException2 {
+        throw new OnOffException1();
+    }
+
+    public static void main(String[] args) {
+
+        try {
+            sw.on();
+            f();
+            sw.off();
+        } catch (OnOffException1 onOffException1) {
+            System.out.println("onOffException1");
+            sw.off();
+            onOffException1.printStackTrace();
+        } catch (OnOffException2 onOffException2) {
+            System.out.println("onOffException2");
+            sw.off();
+            onOffException2.printStackTrace();
+        }
+    }
+
+}
+
+class OnOffException1 extends Exception {
+
+}
+
+class OnOffException2 extends Exception {
+
+}
