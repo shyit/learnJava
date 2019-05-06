45b291dd3027427234bea83bf6d82b636cdf07db
96955 <qwe123>
2019-05-06 17:02:45 +0800

    不可变String和StringBuilder

---
+++ src/net/htwater/think/D13_1/UsingDtringBuilder.java
@@ -0,0 +1,21 @@
+package net.htwater.think.D13_1;
+
+import java.util.Random;
+
+/**
+ * Created by 96955 on 2019/5/6.
+ */
+public class UsingDtringBuilder {
+
+    public static Random random=new Random(47);
+    public String toString(){
+        StringBuilder stringBuilder=new StringBuilder("[");
+        for (int i=0;i<25;i++){
+            stringBuilder.append(random.nextInt(100));
+            stringBuilder.append(", ");
+        }
+        stringBuilder.append("]");
+        return stringBuilder.toString();
+    }
+}
+
