72caecb7038a1b23a8a150876658e594dbaa0680
969553988@qq.com <qwer1234>
2020-08-26 20:52:45 +0800

    循环链表

---
+++ src/net/htwater/algorithm/base/LoopNode.java
@@ -0,0 +1,37 @@
+package net.htwater.algorithm.base;
+
+public class LoopNode {
+    //节点内容
+    int data;
+
+    //下一个节点
+    LoopNode next=this;
+
+    public LoopNode(int data) {
+        this.data = data;
+    }
+
+
+    //获取下一个节点
+    public LoopNode next(){
+        return this.next;
+    }
+
+
+    //删除下一个节点
+    public void removeNext(){
+        this.next=this.next.next;
+//        if (this.next==this){
+//            this=null;
+//        }else {
+//
+//        }
+    }
+
+    //插入节点
+    public void after(LoopNode node){
+        node.next=this.next;
+        this.next=node;
+    }
+
+}
---
+++ src/net/htwater/algorithm/base/LoopNodeTest.java
@@ -0,0 +1,22 @@
+package net.htwater.algorithm.base;
+
+public class LoopNodeTest {
+    public static void main(String[] args) {
+        LoopNode loopNode1=new LoopNode(11);
+        LoopNode loopNode2=new LoopNode(22);
+        LoopNode loopNode3=new LoopNode(33);
+        LoopNode loopNode4=new LoopNode(44);
+
+        loopNode1.after(loopNode2);
+        loopNode2.after(loopNode3);
+        loopNode3.after(loopNode4);
+
+        System.out.println(loopNode1.data);
+        System.out.println(loopNode1.next.data);
+        System.out.println(loopNode1.next.next.data);
+        System.out.println(loopNode1.next.next.next.data);
+        System.out.println(loopNode1.next.next.next.next.data);
+
+
+    }
+}
