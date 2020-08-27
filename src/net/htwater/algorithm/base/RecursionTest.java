7f71acd52c3d1da7e85e0b0896ee31a7857e6e69
969553988@qq.com <qwer1234>
2020-08-27 20:28:06 +0800

    斐波那契数列

---
+++ src/net/htwater/algorithm/base/RecursionTest.java
@@ -0,0 +1,31 @@
+package net.htwater.algorithm.base;
+
+
+public class RecursionTest {
+    public static void main(String[] args) {
+        System.out.println(fibonacci(1));
+        System.out.println(fibonacci(2));
+        System.out.println(fibonacci(3));
+        System.out.println(fibonacci(4));
+        System.out.println(fibonacci(5));
+        System.out.println(fibonacci(6));
+        System.out.println(fibonacci(7));
+        System.out.println(fibonacci(8));
+
+
+    }
+
+    //斐波那契数列
+    private static int fibonacci(int i) {
+        if (i<=0){
+            throw new RuntimeException("下标错误");
+        }
+
+        if (i==1||i==2){
+            return 1;
+        }
+
+        return fibonacci(i-1)+fibonacci(i-2);
+    }
+
+}
