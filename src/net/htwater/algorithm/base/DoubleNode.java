55d89ad06b02bc349985895b6bdf408e8ecc0b39
969553988@qq.com <qwer1234>
2020-08-27 19:56:00 +0800

    双向列表

---
+++ src/net/htwater/algorithm/base/DoubleNode.java
@@ -0,0 +1,37 @@
+package net.htwater.algorithm.base;
+
+public class DoubleNode {
+    //上一个节点
+    DoubleNode pre =this;
+
+    //下一个节点
+    DoubleNode next=this;
+
+    //节点数据
+    int data;
+
+    public DoubleNode(int data) {
+        this.data = data;
+    }
+
+    //增加节点
+    public void after(DoubleNode node){
+        DoubleNode nextNode=this.next;
+
+        node.pre=this;
+        node.next=nextNode;
+
+        nextNode.pre=node;
+        this.next=node;
+    }
+
+    //下一个节点
+    public DoubleNode getNext(){
+        return this.next;
+    }
+
+    //上一个节点
+    public DoubleNode getPre(){
+        return this.pre;
+    }
+}
---
+++ src/net/htwater/algorithm/base/DoubleNodeTest.java
@@ -0,0 +1,25 @@
+package net.htwater.algorithm.base;
+
+public class DoubleNodeTest {
+    public static void main(String[] args) {
+        DoubleNode doubleNode1 = new DoubleNode(11);
+        DoubleNode doubleNode2 = new DoubleNode(22);
+        DoubleNode doubleNode3 = new DoubleNode(33);
+        DoubleNode doubleNode4 = new DoubleNode(44);
+
+        doubleNode1.after(doubleNode2);
+        doubleNode2.after(doubleNode3);
+        doubleNode3.after(doubleNode4);
+
+        System.out.println(doubleNode1.data);
+        System.out.println(doubleNode1.next.data);
+        System.out.println(doubleNode1.next.next.data);
+        System.out.println(doubleNode1.next.next.next.data);
+        System.out.println(doubleNode1.next.next.next.next.data);
+        System.out.println(doubleNode1.next.next.next.next.next.data);
+        System.out.println(doubleNode1.next.next.next.next.next.next.data);
+        System.out.println(doubleNode1.next.next.next.next.next.next.next.data);
+        System.out.println(doubleNode1.next.next.next.next.next.next.next.next.data);
+        System.out.println(doubleNode1.next.next.next.next.next.next.next.next.next.data);
+    }
+}
