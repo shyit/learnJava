148ba3046a462a3a31dbed5afea30f78a2c8e218
96955 <969553988@qq.com>
2023-02-08 16:15:34 +0800

    归并排序 从下到上

--- src/net/htwater/algorithmmuke/sort/mergesort/MergeSort.java
+++ src/net/htwater/algorithmmuke/sort/mergesort/MergeSort.java
@@ -40,12 +40,55 @@ public class MergeSort {
         }
     }
 
+    public static <E extends Comparable> void sort2(E[] arr) {
+
+
+        for (int step = 1; step < arr.length; step += step) {
+            for (int m = 0; m+step < arr.length; m += step * 2) {
+                int start = m;
+                int middle = m + step;
+                int end = m + step * 2 - 1;
+
+                if (end >= arr.length-1) {
+                    end = arr.length-1;
+                }
+
+                E[] tempArr = (E[]) new Comparable[end - start + 1];
+                for (int j = 0; j < tempArr.length; j++) {
+                    tempArr[j] = arr[start + j];
+                }
+
+                int i = start;
+                int j = middle;
+
+                for (int k = start; k <= end; k++) {
+                    if (i >= middle) {
+                        arr[k] = tempArr[j - start];
+                        j++;
+                    } else if (j > end) {
+                        arr[k] = tempArr[i - start];
+                        i++;
+                    } else if (tempArr[i - start].compareTo(tempArr[j - start]) <= 0) {
+                        arr[k] = tempArr[i - start];
+                        i++;
+                    } else {
+                        arr[k] = tempArr[j - start];
+                        j++;
+                    }
+                }
+            }
+        }
+
+
+    }
+
     public static void main(String[] args) {
-        Integer[] nums = new Integer[]{11, 22, 13, 4, 5555, 61, 70, 8, 92, 10};
+        Integer[] nums = new Integer[]{11, 22, 13, 4, 5555, 61, 70, 8, 92, 10,1};
 
         System.out.println(Arrays.toString(nums));
 
-        sort(nums, 0, nums.length - 1);
+//        sort(nums, 0, nums.length - 1);
+        sort2(nums);
 
         System.out.println(Arrays.toString(nums));
     }
