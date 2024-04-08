40bb5e6275c43c76a25366c407ceb8c7c581d374
969553988@qq.com <969553988@qq.com>
2024-04-08 12:49:49 +0800

    LeetcodeNo144. 二叉树的前序遍历

--- src/net/htwater/leetcode/LeetcodeNo144.java
+++ src/net/htwater/leetcode/LeetcodeNo144.java
@@ -5,17 +5,15 @@ import java.util.List;
 import java.util.Stack;
 
 /**
- * 类  名: LeetcodeNo94
- * 描  述: LeetcodeNo94. 二叉树的中序遍历
+ * 类  名: LeetcodeNo144
+ * 描  述: LeetcodeNo144. 二叉树的前序遍历
  * <p>
  * 功  能:
  *
  * @author ShenHaiyun no.HT151 软件部移动组
- * @date 2024-04-07 22:22
+ * @date 2024-04-08 12:20
  */
-public class LeetcodeNo94 {
-
-    // 给定一个二叉树的根节点 root ，返回 它的 中序 遍历 。
+public class LeetcodeNo144 {
     public static void main(String[] args) {
         TreeNode treeNode1 = new TreeNode(1);
         TreeNode treeNode2 = new TreeNode(2);
@@ -24,59 +22,54 @@ public class LeetcodeNo94 {
         treeNode1.right = treeNode2;
         treeNode2.left = treeNode3;
 
-        List<Integer> intList = inorderTraversal2(treeNode1);
+        List<Integer> intList = preorderTraversal2(treeNode1);
 
-        // 1 3 2
+        // 1 2 3
         intList.forEach(item -> System.out.println(item));
     }
 
     // 递归法
-    public static List<Integer> inorderTraversal(TreeNode root) {
+    public static List<Integer> preorderTraversal(TreeNode root) {
         if (root == null) {
             return new ArrayList<>();
         }
 
         List<Integer> intList = new ArrayList<>();
 
-        // 左子树
-        List<Integer> intListLeft = inorderTraversal(root.left);
-        intList.addAll(intListLeft);
-
         // 当前节点
         intList.add(root.val);
 
+        // 左子树
+        List<Integer> integerListLeft = preorderTraversal(root.left);
+        intList.addAll(integerListLeft);
+
         // 右子树
-        List<Integer> intListRight = inorderTraversal(root.right);
-        intList.addAll(intListRight);
+        List<Integer> integerListRight = preorderTraversal(root.right);
+        intList.addAll(integerListRight);
 
         return intList;
     }
 
     // 迭代法
-    public static List<Integer> inorderTraversal2(TreeNode root) {
+    public static List<Integer> preorderTraversal2(TreeNode root) {
         if (root == null) {
             return new ArrayList<>();
         }
 
+        //
         List<Integer> intList = new ArrayList<>();
         Stack<TreeNode> stack = new Stack<>();
 
         TreeNode treeNodeCurrent = root;
         while (treeNodeCurrent != null || !stack.isEmpty()) {
-            // 遍历当前节点及其所有 左孩子
             while (treeNodeCurrent != null) {
+                intList.add(treeNodeCurrent.val);
                 stack.push(treeNodeCurrent);
                 treeNodeCurrent = treeNodeCurrent.left;
             }
 
-            // 栈中取出元素
-            treeNodeCurrent = stack.pop();
-
-            // 赋值为 右孩子
-            intList.add(treeNodeCurrent.val);
-            treeNodeCurrent = treeNodeCurrent.right;
+            treeNodeCurrent = stack.pop().right;
         }
-
 
         return intList;
     }
@@ -99,4 +92,5 @@ public class LeetcodeNo94 {
             this.right = right;
         }
     }
+
 }
